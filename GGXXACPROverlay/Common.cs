37eea18f5260e23c5864b244d9772fc77e24f005
Chase <chaseharland@bellsouth.net>
2025-02-27 02:08:47 -0500

    Adjustments to known issues dialog

--- GGXXACPROverlay/Common.cs
+++ GGXXACPROverlay/Common.cs
@@ -27,15 +27,17 @@
             "- Frame Meter does not fully support Replay mode\n" +
             "- Startup is not fully implemented for EX characters\n" +
             "- Projectile startup is not implemented\n" +
+            "- Startup / advantage will not exceed 77\n" +
             "- Some moves are missing hitboxes (e.g. Slidehead unblockable, Dizzy 421S, Dizzy fish laser)\n" +
-            "- Startup for supers may be incorrect if Baiken guardcancels out of superflash\n" +
-            "- When using \"super flash pausing\" setting, startup will count freeze frames\n";
+            "- Startup for supers may be incorrect if Baiken guard cancels out of super flash\n" +
+            "- When using \"toggle hitstop pausing\" hitstop will be counted in startup for multihit moves\n " +
+            "- When using \"toggle super flash pausing\" setting, startup will count freeze frames\n";
         public const string CONSOLE_CONTROLS =
             "In this console window:\n" +
             "Press '1' to toggle hitbox display\n" +
             "Press '2' to toggle always-on throw range display\n" +
-            " *Air throw boxes only check for the pushbox's bottom edge highlighted in yellow\n\n" +
-            "Press '3' to toggle frame meter display\n" +
+            " *Air throw boxes only check for the pushbox's bottom edge highlighted in yellow\n" +
+            "\nPress '3' to toggle frame meter display\n" +
             "Press '4' to display frame meter legend\n" +
             "Press '5' to toggle frame meter hitstop pausing (good for Slashback practice)\n" +
             "Press '6' to toggle frame meter super flash pausing\n" +
